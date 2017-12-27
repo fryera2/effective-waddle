bb99bcb1886f1967c35176b4a9b53a1400ad98be
Andrew Fryer <andrew_fryer@hotmail.co.uk>
2017-12-27 08:50:41 +0000

    First commit

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,8 +8,43 @@ namespace ConsoleApplication2
 {
     class Program
     {
+
+
+        public class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+        }
         static void Main(string[] args)
         {
+            var cars = new List<Car>
+            {
+                new Car { Make = "A", Model = "Ford" },
+                new Car { Make = "D", Model = "Ford" },
+                new Car { Make = "C", Model = "Cheese" }
+            };
+
+            var results = from c in cars
+                          group c by c.Make into g
+                          orderby g.Key
+                          select g.Key.Max();
+
+        }
+
+        public static bool IsPrime (int input)
+        {
+            bool isPrime = false;
+
+            for (int checkDigit = 2; checkDigit < input; checkDigit++)
+            {
+                if (input % checkDigit == 0)
+                {
+                    isPrime = true;
+                    break;
+                }
+            }
+
+            return isPrime;
         }
     }
 }
